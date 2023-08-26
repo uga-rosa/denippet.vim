3c2c99e880fa46a3816d29c3825219aa4eb410cb
uga-rosa <uga6603@gmail.com>
2023-08-26 18:31:20 +0900

    Add features to Node

--- denops/denippet/parser/node.ts
+++ denops/denippet/parser/node.ts
@@ -1,4 +1,6 @@
-import { camelcase } from "../deps.ts";
+import { api, camelcase, Denops, LSP, lsputil } from "../deps.ts";
+
+export const NAMESPACE = "denippet_jumpable_node";
 
 export type NodeType =
   | "snippet"
@@ -17,16 +19,50 @@ export type FormatModifier =
   | "camelcase"
   | "pascalcase";
 
+// 0-indexed, utf-16 offset
+function calcRange(start: LSP.Position, text: string): LSP.Range {
+  const lines = text.replaceAll(/\r\n?/g, "\n").split("\n");
+  const endLine = start.line + lines.length - 1;
+  const endCharacter = lines.length > 1
+    ? lines[lines.length - 1].length
+    : start.character + text.length;
+  return { start, end: { line: endLine, character: endCharacter } };
+}
+
+function isSamePosition(a: LSP.Position, b: LSP.Position): boolean {
+  return a.line === b.line && a.character === b.character;
+}
+
+function isSameRange(a: LSP.Range, b: LSP.Range): boolean {
+  return isSamePosition(a.start, b.start) && isSamePosition(a.end, b.end);
+}
+
 export abstract class Node {
   abstract type: NodeType;
-  copy?: Jumpable;
+  abstract denops: Denops;
+  range?: LSP.Range;
 
   isJumpable(): this is Jumpable {
     return ["tabstop", "placeholder", "choice"].includes(this.type);
   }
-}
 
-export type Jumpable = Tabstop | Placeholder | Choice;
+  getText(): string {
+    return "";
+  }
+
+  /** 0-indexed, utf-16 offset */
+  async updateRange(start: LSP.Position): Promise<LSP.Position> {
+    const text = this.getText();
+    const newRange = calcRange(start, text);
+    if (this.range && !isSameRange(this.range, newRange)) {
+      const text = this.getText();
+      const replacement = text.replaceAll(/\r\n?/g, "\n").split("\n");
+      await lsputil.setText(this.denops, 0, this.range, replacement);
+    }
+    this.range = newRange;
+    return this.range.end;
+  }
+}
 
 export class Snippet extends Node {
   type: "snippet" = "snippet";
@@ -39,53 +75,237 @@ export class Snippet extends Node {
       | Variable
       | Text
     )[],
+    public denops: Denops,
   ) {
     super();
   }
+
+  getText(): string {
+    return this.children.map((child) => child.getText()).join("");
+  }
+
+  async updateRange(start?: LSP.Position): Promise<LSP.Position> {
+    if (!start && !this.range) {
+      throw new Error("Internal error: Node.Snippet.updateRange");
+    }
+    let pos = start ?? this.range!.start;
+    for (const child of this.children) {
+      pos = await child.updateRange(pos);
+    }
+    return pos;
+  }
 }
 
-export class Tabstop extends Node {
+export abstract class Jumpable extends Node {
+  input?: string;
+  copy?: Jumpable;
+  extmarkId?: number;
+  abstract tabstop: number;
+  abstract getPriority(): number;
+
+  #nsId?: number;
+  async nsId(): Promise<number> {
+    if (this.#nsId == null) {
+      this.#nsId = await api.nvim_create_namespace(
+        this.denops,
+        NAMESPACE,
+      ) as number;
+    }
+    return this.#nsId;
+  }
+
+  // Fire on TextChangedI
+  async updateInput(): Promise<void> {
+    if (this.extmarkId == null) {
+      return;
+    }
+    const [
+      row,
+      col,
+      { end_row, end_col },
+    ] = await api.nvim_buf_get_extmark_by_id(
+      this.denops,
+      0,
+      await this.nsId(),
+      this.extmarkId,
+      { details: true },
+    ) as [number, number, { end_row: number; end_col: number }];
+    if (row == null) {
+      // Invalid extmark id
+      return;
+    }
+    const range8 = lsputil.createRange(row, col, end_row, end_col);
+    const range16 = await lsputil.toUtf16Range(this.denops, 0, range8, "utf-8");
+    const lines = await lsputil.getText(
+      this.denops,
+      0,
+      range16,
+    );
+    this.input = lines.join("\n");
+  }
+
+  async setExtmark(): Promise<void> {
+    if (!this.range) {
+      throw new Error("Internal error: Node.Jumpable.setExtmark");
+    }
+    this.extmarkId = await api.nvim_buf_set_extmark(
+      this.denops,
+      0,
+      await this.nsId(),
+      this.range.start.line,
+      this.range.start.character,
+      {
+        end_row: this.range.end.line,
+        end_col: this.range.end.character,
+        right_gravity: false,
+        end_right_gravity: true,
+      },
+    ) as number;
+  }
+
+  async drop(): Promise<void> {
+    const nsId = await this.nsId();
+    await api.nvim_buf_clear_namespace(this.denops, 0, nsId, 0, -1);
+  }
+
+  async jump(): Promise<void> {
+    if (!this.range) {
+      throw new Error("Internal error: Node.Jumpable.jump");
+    }
+    await this.setExtmark();
+    const range = this.range;
+    if (isSamePosition(range.start, range.end)) {
+      await lsputil.setCursor(this.denops, range.start);
+      return;
+    }
+    await this.denops.cmd("stopinsert");
+    const range8 = await lsputil.toUtf8Range(this.denops, 0, range, "utf-16");
+    await this.denops.call("denippet#select", range8);
+  }
+}
+
+export class Tabstop extends Jumpable {
   type: "tabstop" = "tabstop";
+  transformer?: (input: string) => string;
 
   constructor(
+    public denops: Denops,
     public tabstop: number,
-    public transform?: Transform,
+    transform?: Transform,
   ) {
     super();
+
+    this.transformer = transform?.transformer;
+  }
+
+  getText(): string {
+    if (this.input != null) {
+      return this.input;
+    } else if (this.copy) {
+      let text = this.copy.getText();
+      if (this.transformer) {
+        text = this.transformer(text);
+      }
+      return text;
+    } else {
+      return "";
+    }
+  }
+
+  getPriority(): number {
+    return this.transformer ? -1 : 0;
   }
 }
 
-export class Placeholder extends Node {
+export class Placeholder extends Jumpable {
   type: "placeholder" = "placeholder";
 
   constructor(
+    public denops: Denops,
     public tabstop: number,
     public children: Snippet["children"],
   ) {
     super();
   }
+
+  getText(): string {
+    if (this.input != null) {
+      return this.input;
+    } else if (this.copy) {
+      return this.copy.getText();
+    } else {
+      return this.children.map((child) => child.getText()).join("");
+    }
+  }
+
+  getPriority(): number {
+    return this.children.length > 0 ? 1 : 0;
+  }
+
+  async updateRange(start: LSP.Position): Promise<LSP.Position> {
+    let pos = start;
+    for (const child of this.children) {
+      pos = await child.updateRange(pos);
+    }
+    return pos;
+  }
 }
 
-export class Choice extends Node {
+export class Choice extends Jumpable {
   type: "choice" = "choice";
+  index = 0;
 
   constructor(
+    public denops: Denops,
     public tabstop: number,
     public items: string[],
   ) {
     super();
   }
+
+  getText(): string {
+    return this.items[this.index] ?? "";
+  }
+
+  getPriority(): number {
+    return 1;
+  }
+
+  selectNext(): void {
+    this.index++;
+    if (this.index >= this.items.length) {
+      this.index = 0;
+    }
+  }
+
+  selectPrev(): void {
+    this.index--;
+    if (this.index < 0) {
+      this.index = this.items.length - 1;
+    }
+  }
 }
 
 export class Variable extends Node {
   type: "variable" = "variable";
+  transformer: (input: string) => string;
 
   constructor(
+    public denops: Denops,
     public name: string,
-    public transform?: Transform,
+    transform?: Transform,
     public children?: Snippet["children"],
   ) {
     super();
+
+    this.transformer = transform
+      ? transform.transformer
+      : (input: string) => input;
+  }
+
+  getText(): string {
+    // TODO
+    return "";
   }
 }
 
@@ -95,6 +315,7 @@ export class Transform extends Node {
   formats: (Format | Text)[];
 
   constructor(
+    public denops: Denops,
     pattern: string,
     formats: (Format | Text)[],
     option?: string,
@@ -130,6 +351,7 @@ export class Format extends Node {
   type: "format" = "format";
 
   constructor(
+    public denops: Denops,
     public captureIndex: number,
     public modifier?: FormatModifier,
     public ifText?: string,
@@ -161,16 +383,13 @@ export class Format extends Node {
 
     return str;
   }
-
-  getText(): string {
-    return "";
-  }
 }
 
 export class Text extends Node {
   type: "text" = "text";
 
   constructor(
+    public denops: Denops,
     public text: string,
   ) {
     super();
