5e98ff4958b1144cce95eea67d263019f621a183
uga-rosa <uga6603@gmail.com>
2023-08-26 18:31:58 +0900

    Adjust indents

---
+++ denops/denippet/indent.ts
@@ -0,0 +1,45 @@
+// https://github.com/hrsh7th/vim-vsnip/blob/7753ba9c10429c29d25abfd11b4c60b76718c438/autoload/vsnip/indent.vim
+// Copyright (c) 2019 hrsh7th
+
+import { Denops, fn, op } from "./deps.ts";
+
+async function getOneIndent(
+  denops: Denops,
+): Promise<string> {
+  if (await op.expandtab.get(denops)) {
+    let width = await op.shiftwidth.get(denops);
+    if (width === 0) {
+      width = await op.tabstop.get(denops);
+    }
+    return " ".repeat(width);
+  } else {
+    return "\t";
+  }
+}
+
+async function getBaseIndent(
+  denops: Denops,
+): Promise<string> {
+  const line = await fn.getline(denops, ".");
+  return line.match(/^\s*/)?.[0] ?? "";
+}
+
+export async function adjustIndent(
+  denops: Denops,
+  text: string,
+): Promise<string> {
+  const oneIndent = await getOneIndent(denops);
+  const baseIndent = await getBaseIndent(denops);
+  if (oneIndent !== "\t") {
+    text = text.replaceAll(
+      /(?<=^|\n)\t+/g,
+      (match) => oneIndent.repeat(match.length),
+    );
+  }
+  // Add baseIndent to all lines except the first line.
+  text = text.replaceAll(/\n/g, `\n${baseIndent}`);
+  // Remove indentation on all blank lines except the last line.
+  text = text.replaceAll(/\n\s*\n/g, "\n\n");
+
+  return text;
+}
