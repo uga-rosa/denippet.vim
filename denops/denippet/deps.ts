10d2629a7cbd4a1df92fc8e0cb540827be194de0
uga-rosa <uga6603@gmail.com>
2023-08-30 14:19:28 +0900

    Update deps.ts

--- denops/denippet/deps.ts
+++ denops/denippet/deps.ts
@@ -1,11 +1,14 @@
+export * as TOML from "https://deno.land/std@0.200.0/toml/mod.ts";
+export * as YAML from "https://deno.land/std@0.200.0/yaml/mod.ts";
+
 export type { Denops } from "https://deno.land/x/denops_std@v5.0.1/mod.ts";
 export * as fn from "https://deno.land/x/denops_std@v5.0.1/function/mod.ts";
 export * as api from "https://deno.land/x/denops_std@v5.0.1/function/nvim/mod.ts";
 export * as op from "https://deno.land/x/denops_std@v5.0.1/option/mod.ts";
 
-export * as u from "https://deno.land/x/unknownutil@v3.4.0/mod.ts";
+export * as u from "https://deno.land/x/unknownutil@v3.6.0/mod.ts";
 
 export * as lsputil from "https://deno.land/x/denops_lsputil@v0.5.4/mod.ts";
 export { LSP } from "https://deno.land/x/denops_lsputil@v0.5.4/deps.ts";
 
-export { default as camelcase } from "npm:camelcase@7.0.1";
+export { default as camelcase } from "npm:camelcase@8.0.0";
