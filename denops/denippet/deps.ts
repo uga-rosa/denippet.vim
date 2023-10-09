fd4a569ea85de57eded3fa4b9260871b1f41ae12
uga-rosa <uga6603@gmail.com>
2023-10-09 14:39:37 +0900

    Update dependencies

--- denops/denippet/deps.ts
+++ denops/denippet/deps.ts
@@ -1,5 +1,5 @@
-export * as TOML from "https://deno.land/std@0.201.0/toml/mod.ts";
-export * as YAML from "https://deno.land/std@0.201.0/yaml/mod.ts";
+export * as TOML from "https://deno.land/std@0.203.0/toml/mod.ts";
+export * as YAML from "https://deno.land/std@0.203.0/yaml/mod.ts";
 
 export type { Denops } from "https://deno.land/x/denops_std@v5.0.1/mod.ts";
 export * as fn from "https://deno.land/x/denops_std@v5.0.1/function/mod.ts";
@@ -12,7 +12,7 @@ export { echoerr } from "https://deno.land/x/denops_std@v5.0.1/helper/mod.ts";
 export * as u from "https://deno.land/x/unknownutil@v3.9.0/mod.ts";
 export { is } from "https://deno.land/x/unknownutil@v3.9.0/mod.ts";
 
-export * as lsputil from "https://deno.land/x/denops_lsputil@v0.5.4/mod.ts";
-export { LSP } from "https://deno.land/x/denops_lsputil@v0.5.4/deps.ts";
+export * as lsputil from "https://deno.land/x/denops_lsputil@v0.7.0/mod.ts";
+export { LSP } from "https://deno.land/x/denops_lsputil@v0.7.0/deps.ts";
 
 export { default as camelcase } from "npm:camelcase@8.0.0";
