b11b80a70e69e62be1b43feb0d67cdf42b87d987
uga-rosa <uga6603@gmail.com>
2024-02-25 15:52:27 +0900

    udd

--- denops/denippet/deps/std.ts
+++ denops/denippet/deps/std.ts
@@ -1,4 +1,4 @@
-export * as TOML from "https://deno.land/std@0.210.0/toml/mod.ts";
-export * as YAML from "https://deno.land/std@0.210.0/yaml/mod.ts";
-export * as path from "https://deno.land/std@0.210.0/path/mod.ts";
-export { assert, assertEquals } from "https://deno.land/std@0.210.0/assert/mod.ts";
+export * as TOML from "https://deno.land/std@0.217.0/toml/mod.ts";
+export * as YAML from "https://deno.land/std@0.217.0/yaml/mod.ts";
+export * as path from "https://deno.land/std@0.217.0/path/mod.ts";
+export { assert, assertEquals } from "https://deno.land/std@0.217.0/assert/mod.ts";
