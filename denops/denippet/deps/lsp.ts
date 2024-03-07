c0a74b0f84069bbd73f4676da72e3920d9560f39
uga-rosa <uga6603@gmail.com>
2024-03-07 23:13:12 +0900

    udd

--- denops/denippet/deps/lsp.ts
+++ denops/denippet/deps/lsp.ts
@@ -1,3 +1,3 @@
-export * as lsputil from "https://deno.land/x/denops_lsputil@v0.9.3/mod.ts";
-export * as LN from "https://deno.land/x/denops_lsputil@v0.9.3/snippet/node.ts";
-export { LSP } from "https://deno.land/x/denops_lsputil@v0.9.3/deps.ts";
+export * as lsputil from "https://deno.land/x/denops_lsputil@v0.9.4/mod.ts";
+export * as LN from "https://deno.land/x/denops_lsputil@v0.9.4/snippet/node.ts";
+export { LSP } from "https://deno.land/x/denops_lsputil@v0.9.4/deps.ts";
