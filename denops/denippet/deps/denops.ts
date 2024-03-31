1037eac16214d3fa72b944e18c15b46636eb8059
uga-rosa <uga6603@gmail.com>
2024-03-31 16:36:38 +0900

    udd

--- denops/denippet/deps/denops.ts
+++ denops/denippet/deps/denops.ts
@@ -1,13 +1,16 @@
-export type { Denops } from "https://deno.land/x/denops_std@v6.3.0/mod.ts";
-export * as fn from "https://deno.land/x/denops_std@v6.3.0/function/mod.ts";
-export * as api from "https://deno.land/x/denops_std@v6.3.0/function/nvim/mod.ts";
-export * as vim from "https://deno.land/x/denops_std@v6.3.0/function/vim/mod.ts";
-export * as op from "https://deno.land/x/denops_std@v6.3.0/option/mod.ts";
-export { b, g } from "https://deno.land/x/denops_std@v6.3.0/variable/variable.ts";
-export * as au from "https://deno.land/x/denops_std@v6.3.0/autocmd/mod.ts";
-export * as lambda from "https://deno.land/x/denops_std@v6.3.0/lambda/mod.ts";
-export { batch } from "https://deno.land/x/denops_std@v6.3.0/batch/mod.ts";
-export * as Keymap from "https://deno.land/x/denops_std@v6.3.0/helper/keymap.ts";
-export { exprQuote as q } from "https://deno.land/x/denops_std@v6.3.0/helper/expr_string.ts";
+export type { Denops } from "https://deno.land/x/denops_std@v6.4.0/mod.ts";
+export * as fn from "https://deno.land/x/denops_std@v6.4.0/function/mod.ts";
+export * as api from "https://deno.land/x/denops_std@v6.4.0/function/nvim/mod.ts";
+export * as vim from "https://deno.land/x/denops_std@v6.4.0/function/vim/mod.ts";
+export * as op from "https://deno.land/x/denops_std@v6.4.0/option/mod.ts";
+export { b, g } from "https://deno.land/x/denops_std@v6.4.0/variable/variable.ts";
+export * as au from "https://deno.land/x/denops_std@v6.4.0/autocmd/mod.ts";
+export * as lambda from "https://deno.land/x/denops_std@v6.4.0/lambda/mod.ts";
+export { batch } from "https://deno.land/x/denops_std@v6.4.0/batch/mod.ts";
+export * as Keymap from "https://deno.land/x/denops_std@v6.4.0/helper/keymap.ts";
+export {
+  exprQuote as q,
+  type ExprString,
+} from "https://deno.land/x/denops_std@v6.4.0/helper/expr_string.ts";
 
 export { test } from "https://deno.land/x/denops_test@v1.6.2/mod.ts";
--- denops/denippet/deps/std.ts
+++ denops/denippet/deps/std.ts
@@ -1,4 +1,5 @@
-export * as TOML from "https://deno.land/std@0.218.2/toml/mod.ts";
-export * as YAML from "https://deno.land/std@0.218.2/yaml/mod.ts";
-export * as path from "https://deno.land/std@0.218.2/path/mod.ts";
-export { assert, assertEquals } from "https://deno.land/std@0.218.2/assert/mod.ts";
+export * as TOML from "https://deno.land/std@0.221.0/toml/mod.ts";
+export * as YAML from "https://deno.land/std@0.221.0/yaml/mod.ts";
+export * as path from "https://deno.land/std@0.221.0/path/mod.ts";
+export { assert, assertEquals } from "https://deno.land/std@0.221.0/assert/mod.ts";
+export { deadline } from "https://deno.land/std@0.221.0/async/mod.ts";
