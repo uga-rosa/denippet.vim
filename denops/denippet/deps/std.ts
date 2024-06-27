9a19bb6ddcdeb7a60860ac27e180d6d400ab10c7
tsuyoshi.nakai <tsuyoshi.nakai@dena.com>
2024-06-27 20:42:57 +0900

    molt

--- denops/denippet/deps/std.ts
+++ denops/denippet/deps/std.ts
@@ -1,4 +1,4 @@
 export * as TOML from "jsr:@std/toml@0.224.1";
-export * as YAML from "jsr:@std/yaml@0.224.2";
+export * as YAML from "jsr:@std/yaml@0.224.3";
 export * as path from "jsr:@std/path@0.225.2";
 export { assert, assertEquals } from "jsr:@std/assert@0.226.0";
